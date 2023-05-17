baff1ea3a0abd15f6380ff5863b47ca12d880e96
GRL49 <128386566+GRL49@users.noreply.github.com>
2023-05-17 10:58:35 +0300

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,67 @@
+from matplotlib import pyplot
+import numpy
+from textwrap import wrap
+import matplotlib.ticker as ticker
+
+
+
+file = open('/home/b01-206/Desktop/Scripts/6667/data.txt', 'r')
+file = open('/home/b01-206/Desktop/Scripts/6667/settings.txt', 'r')
+
+with open('/home/b01-206/Desktop/Scripts/6667/settings.txt') as file:
+    settings=[float(i) for i in file.read().split('\n')]
+
+
+data=numpy.loadtxt('data.txt', dtype=int) * settings[1]
+
+data_time=numpy.array([i*settings[0] for i in range(data.size)])
+
+
+fig, ax=pyplot.subplots(figsize=(16, 10), dpi=500)
+
+
+ax.axis([data.min(), data_time.max()+1, data.min(), data.max()+0.2])
+
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+
+
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+
+ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+
+
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+
+ax.set_title("\n".join(wrap('Процесс заряда и разряда конденсатора в RC цепи', 60)), loc = 'center')
+
+
+ax.grid(which='major', color = 'k')
+ax.minorticks_on()
+ax.grid(which='minor', color = 'gray', linestyle = ':')
+
+
+
+#подпись осей
+ax.set_ylabel("Напряжение U, В")
+ax.set_xlabel("Время t, с")
+
+#линия с легендой
+
+ax.plot(data_time, data, color='black', linewidth=2, label = '$U_C(t)$', marker='o', markerfacecolor='green', linestyle='-',
+    markersize=5, markevery = 10)
+
+textstr = '\n'.join((
+    r'Время зарядки t_{зарядки} = 5.21 c',
+    r'Время разрядки t_{разрядки} = 6.79 c' ))
+
+
+ax.text(0.55, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top')
+
+
+ax.legend(shadow = False, loc = 'right', fontsize = 20)
+
+#сохранение
+fig.savefig('graph.png')
+fig.savefig('graph.svg')
